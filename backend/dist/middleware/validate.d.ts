e8a2cb1e4a14fca3d53fbb96404d25ec24db5c37
Tasavvuf Gori <tasavvufg@gmail.com>
2026-09-17 21:55:50 +0530

    feat: restyle glassmorphism, implement 4-step requirement flow, add events management directory & clean gitignore

--- backend/dist/middleware/validate.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { ZodSchema } from "zod";
-export declare const validate: (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateQuery: (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=validate.d.ts.map
